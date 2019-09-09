ddb9576554c5d0de89c102c65008fd52aed20b66
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-09 14:54:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction(data)
-  puts "Hi #{data}."
+def introduction(name)
+  puts "Hi #{name}."
+end
+
+def introduction_with_language(name,language)
+  puts "Hi #{name}, I so you speak fluent #{language}."
 end
